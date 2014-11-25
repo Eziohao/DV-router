7aec9707eff08bd3321685767de3bb0d66909d0d
eziohao <eziohao@gmail.com>
2014-11-25 00:47:46 -0500

    add DV

--- router_a.js
+++ router_a.js
@@ -1,9 +1,49 @@
 var app = require('express')(); //get express model
 var http = require('http').Server(app); //create a http server
 var io = require('socket.io')(http); //create socket.io
-var router_name = 'A'; //to store login router_name
+
+var router_name = 'a'; //to store login router_name
 var DV = {};
-var router=new Array;
+var a_DV = {
+	"b": {
+		"sID": 'a',
+		"dID": 'b',
+		"dP": 3721,
+		"nH": 1,
+		"dis": 1,
+		"nR": 'b'
+	},
+	"c": {
+		"sID": 'a',
+		"dID": 'c',
+		"dP": 2785,
+		"nH": 1,
+		"dis": 2,
+		"nR": 'c'
+	}
+};
+var b_DV = {
+	"d": {
+		"sID": 'b',
+		"dID": 'd',
+		"dP": 5834,
+		"nH": 1,
+		"dis": 3,
+		"nR": 'd'
+	}
+};
+var c_DV = {
+	"d": {
+		"sID": 'c',
+		"dID": 'd',
+		"dP": 5834,
+		"nH": 1,
+		"dis": 3,
+		"nR": 'd'
+	}
+};
+
+var router = new Array;
 var name;
 var port;
 var dgram = require('dgram'); // UDP需要引入该模块
@@ -14,12 +54,83 @@ app.get('/', function(req, res) { //link the js to html file
 	res.sendfile('index.html');
 
 });
-function findrouters(name){
-	for(var i=0;i<=router.length;i++){
-		if(router[i]==name){
-			return true;
+
+function routing(b_DV, c_DV) {
+	var temp = {};
+	var temp1 = {};
+	for (var item in a_DV) {
+		for (var i in b_DV) {
+			if (item == b_DV[i].sID) {
+				console.log('find');
+				temp[i] = {
+
+					"sID": b_DV[i].sID,
+					"dID": b_DV[i].dID,
+					"dP": b_DV[i].dP,
+					"nH": b_DV[i].nH,
+					"dis": b_DV[i].dis,
+					"nR": b_DV[i].nR
+
+				};
+
+			}
 		}
-		else{
+	}
+	for (var item in a_DV) {
+		for (var i in c_DV) {
+			if (item == c_DV[i].sID) {
+				console.log('got');
+				temp1[i] = {
+
+					"sID": c_DV[i].sID,
+					"dID": c_DV[i].dID,
+					"dP": c_DV[i].dP,
+					"nH": c_DV[i].nH,
+					"dis": c_DV[i].dis,
+					"nR": c_DV[i].nR
+
+				};
+			}
+		}
+	}
+	for (var item in temp) {
+		for (var i in temp1) {
+			if (item == i) {
+				console.log('haha');
+				if (temp[item].dis + a_DV[temp[item].sID].dis >= a_DV[temp1[i].sID].dis + temp1[i].dis) {
+					a_DV[i] = {
+						"sID": router_name,
+						"dID": temp1[i].dID,
+						"dP": temp1[i].dP,
+						"nH": temp1[i].nH + 1,
+						"dis": temp1[i].dis + a_DV[temp1[i].sID].dis,
+						"nR": i
+					}
+					console.log(a_DV);
+				}
+				else{
+				   	a_DV[i] = {
+						"sID": router_name,
+						"dID": temp[item].dID,
+						"dP": temp[item].dP,
+						"nH": temp[item].nH + 1,
+						"dis": temp[item].dis + a_DV[temp[item].sID].dis,
+						"nR": temp[item].sID
+					}
+					console.log(a_DV);
+				}
+			}
+		}
+	}
+
+};
+routing(b_DV, c_DV);
+
+function findrouters(name) {
+	for (var i in router) {
+		if (i == name) {
+			return true;
+		} else {
 			return false;
 		}
 	}
@@ -31,62 +142,70 @@ io.on('connection', function(socket) { //if a user coonect the server
 		msg = 'which router cost you want to add ?'
 		io.emit('add_what', msg);
 	});
-	socket.on('change',function(msg){
-          console.log('change');
-          msg = 'which router cost you want to change ?';
-          io.emit('change_what',msg);
+	socket.on('change', function(msg) {
+		console.log('change');
+		msg = 'which router cost you want to change ?';
+		io.emit('change_what', msg);
 	});
 	socket.on('routers', function(msg) {
 		console.log('chose router');
 		if (!findrouters(msg)) {
-			name=msg;
-			router[msg]=new Array;
+			name = msg;
+			router[msg] = new Array;
 			console.log(router);
-		var	msg = 'Ok, port number?';
+			var msg = 'Ok, port number?';
 			io.emit('add_port', msg);
-		}
-		else{
-			var msg='There has been a same router';
-			io.emit('message',msg);
+		} else {
+			var msg = 'There has been a same router';
+			io.emit('message', msg);
 		}
 
 	});
-	socket.on('kill',function(msg){
-         var msg='router'+' '+router_name+' '+"is killed";
-         io.emit('message',msg);
-         process.exit(0);
+	socket.on('kill', function(msg) {
+		var msg = 'router' + ' ' + router_name + ' ' + "is killed";
+		io.emit('message', msg);
+		process.exit(0);
 	});
-	socket.on('change_routers',function(msg){
-		name=msg;
+	socket.on('change_routers', function(msg) {
+		name = msg;
 		console.log('change routers');
-		var msg='Ok,  port number ?';
-		io.emit('add_port',msg);
+		var msg = 'Ok,  port number ?';
+		io.emit('add_port', msg);
 
 	})
 	socket.on('port', function(msg) {
 		console.log('add port');
-		port=msg;
-		var msg="How many cost ?"
+		port = msg;
+		var msg = "How many cost ?"
 		io.emit('add_cost', msg);
 
 	});
-	socket.on('cost',function(msg){
-        console.log('add cost');
-        router[name][port]=msg;
-        
-        DV[name]={"sID":'a',"dID":name,"dP":port,"nH":1,"dis":msg,"nR":name};
-       /* DV.name.sID='a';
-        DV.name.dID=name;
-        DV.name.dP=port;
-        DV.name.nH=1;
-        DV.name.dis=msg;
-        DV.name.nR=name;*/
-        var msg=name+" "+"is set";
-        console.log(router[name][port]);
-        console.log(DV);
-        io.emit('message',msg);
-	})
-	
+	socket.on('cost', function(msg) {
+		console.log('add cost');
+		router[name][port] = msg;
+
+		DV[name] = {
+			"sID": 'a',
+			"dID": name,
+			"dP": port,
+			"nH": 1,
+			"dis": msg,
+			"nR": name
+		};
+		/* DV.name.sID='a';
+		 DV.name.dID=name;
+		 DV.name.dP=port;
+		 DV.name.nH=1;
+		 DV.name.dis=msg;
+		 DV.name.nR=name;*/
+		var msg = name + " " + "is set";
+		console.log(router[name][port]);
+		console.log(DV);
+		var s = DV;
+		io.emit('message', s);
+	});
+
+
 });
 
 
@@ -96,6 +215,3 @@ http.listen(8081, function() { //The router will listen commands from port 2547
 
 
 });
-
-
-
--- router_b.js
+++ router_b.js
@@ -1,33 +1,102 @@
-var PORT1 = 8888;
-var HOST = '127.0.0.1';
+var app = require('express')(); //get express model
+var http = require('http').Server(app); //create a http server
+var io = require('socket.io')(http); //create socket.io
+var router_name = 'B'; //to store login router_name
+var DV = {};
+var router=new Array;
+var name;
+var port;
+var dgram = require('dgram'); // UDP需要引入该模块
+var server = dgram.createSocket('udp4'); // ipv4
+var server1 = dgram.createSocket('udp4');
 
-var dgram = require('dgram');
-var json = '{"result":true,"count":1}',
-    obj = JSON.parse(json);
+app.get('/', function(req, res) { //link the js to html file
+	res.sendfile('index.html');
 
-
-//var msg=new Buffer(JSON.parse(message));
-
-
-var json1 = JSON.stringify(json);
-
-console.log(json1);
-// '[116,101,115,116]'
-
-var copy = new Buffer(json1);
-
-console.log(copy);
-
-var message1=new Buffer('hhhhhh');
-var client = dgram.createSocket('udp4'); // ipv4
-var client1=dgram.createSocket('udp4');
-client.send(copy, 0, copy.length, PORT1, HOST, function(err, bytes) {
-    if (err) throw err;
-    console.log('UDP message sent to ' + HOST +':'+ PORT1);
-    client.close();
 });
-client1.send(message1, 0, message1.length, 8080, HOST, function(err, bytes) {
-    if (err) throw err;
-    console.log('UDP message sent to ' + HOST +':'+ 8080);
-    client1.close();
+function findrouters(name){
+	for(var i=0;i<=router.length;i++){
+		if(router[i]==name){
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
+}
+io.on('connection', function(socket) { //if a user coonect the server
+	console.log('connect to the control');
+	socket.on('add', function(msg) {
+		console.log('add');
+		msg = 'which router cost you want to add ?'
+		io.emit('add_what', msg);
+	});
+	socket.on('change',function(msg){
+          console.log('change');
+          msg = 'which router cost you want to change ?';
+          io.emit('change_what',msg);
+	});
+	socket.on('routers', function(msg) {
+		console.log('chose router');
+		if (!findrouters(msg)) {
+			name=msg;
+			router[msg]=new Array;
+			console.log(router);
+		var	msg = 'Ok, port number?';
+			io.emit('add_port', msg);
+		}
+		else{
+			var msg='There has been a same router';
+			io.emit('message',msg);
+		}
+
+	});
+	socket.on('kill',function(msg){
+         var msg='router'+' '+router_name+' '+"is killed";
+         io.emit('message',msg);
+         process.exit(0);
+	});
+	socket.on('change_routers',function(msg){
+		name=msg;
+		console.log('change routers');
+		var msg='Ok,  port number ?';
+		io.emit('add_port',msg);
+
+	})
+	socket.on('port', function(msg) {
+		console.log('add port');
+		port=msg;
+		var msg="How many cost ?"
+		io.emit('add_cost', msg);
+
+	});
+	socket.on('cost',function(msg){
+        console.log('add cost');
+        router[name][port]=msg;
+        
+        DV[name]={"sID":'b',"dID":name,"dP":port,"nH":1,"dis":msg,"nR":name};
+       /* DV.name.sID='a';
+        DV.name.dID=name;
+        DV.name.dP=port;
+        DV.name.nH=1;
+        DV.name.dis=msg;
+        DV.name.nR=name;*/
+        var msg=name+" "+"is set";
+        console.log(router[name][port]);
+        console.log(DV);
+        io.emit('message',msg);
+	});
+	
+	
 });
+
+
+
+http.listen(8082, function() { //The router will listen commands from port 2547
+	console.log('Routers starts on 8082');
+
+
+});
+
+
+

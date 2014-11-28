9df2f803bbd90967a0a8d43eb61780272423176a
Eziohao <eziohao@gmail.com>
2014-11-28 00:50:41 -0600

    make dv algorithm right
    rewrite routing function now the dv algorithm is right and well

--- router_a.js
+++ router_a.js
@@ -4,39 +4,49 @@ var io = require('socket.io')(http); //create socket.io
 
 var router_name = 'a'; //to store login router_name
 var router_port = 2547;
-var b_port = 3721;
-var c_port = 4568;
 var DV = {};
-var temp = {};
+var temp = {'d':{"sID": router_name,
+			"dID": 'd',
+			"dP": '5834',
+			"nH": 1,
+			"dis": 3,
+			"nR": 'd'}};
 
 var router = new Array;
 var name;
 var port;
-var dgram = require('dgram'); // UDP需要引入该模块
+var dgram = require('dgram'); // UDP module
 var server = dgram.createSocket('udp4'); // ipv4
-var server1 = dgram.createSocket('udp4');
+var client=dgram.createSocket('udp4');
 
 app.get('/', function(req, res) { //link the js to html file
 	res.sendfile('index.html');
 
 });
-
+//console.log(isEmpty(DV['d']));
 function routing(s_DV) {
 	if (DV != {}) {
 		for (var item in s_DV) {
 			for (var i in DV) {
-				if (item != i && item != router_name && DV[item] == {}) {
+				if (item != i && item != router_name && isEmpty(DV[item])) {
+					console.log('get it');
 					DV[item] = {
 						"sID": router_name,
 						"dID": s_DV[item].dID,
 						"dP": s_DV[item].dP,
 						"nH": s_DV[item].nH + DV[s_DV[item].sID].nH,
-						"dis": s_DV[item].dis + DV[s_DV.sID].dis,
+						"dis": parseInt(s_DV[item].dis )+ parseInt(DV[s_DV[item].sID].dis),
 						"nR": s_DV[item].sID
 					};
-				} else if (item != i && item != router_name && DV[item] != {}) {
-					if (DV[item].nR != s_DV[item].sID) {
-                          
+				} else if (item != i && item != router_name && !isEmpty(DV[item])) {
+					console.log('have it');
+					if (DV[item].nR!= s_DV[item].sID) {
+						if(parseInt(DV[item].dis) >= parseInt(s_DV[item].dis) + parseInt(DV[s_DV[item].sID].dis)) {
+							console.log('change cost');
+							DV[item].dis = parseInt(s_DV[item].sID) + parseInt(DV[s_DV[item].sID].dis);
+							DV[item].nH = DV[s_DV[item].sID].nH + s_DV[item].nH;
+							DV[item].nH = s_DV[item].sID;
+						}
 					}
 				}
 			}
@@ -53,6 +63,14 @@ function findrouters(name) {
 			return false;
 		}
 	}
+};
+function isEmpty(obj) {
+    for(var prop in obj) {
+        if(obj.hasOwnProperty(prop))
+            return false;
+    }
+ 
+    return true;
 }
 io.on('connection', function(socket) { //if a user coonect the server
 	console.log('connect to the control');
@@ -127,13 +145,13 @@ server.on('listening', function() {
 server.on('message', function(message) {
 	var s_DV = {};
 	s_DV = JSON.parse(message);
-
-	console.log(temp);
+     routing(s_DV);
+	console.log(DV);
 })
 
 
 
-server.bind(2547, '127.0.0.1');
+server.bind(router_port, '127.0.0.1');
 
 
 http.listen(8081, function() { //The router will listen commands from port 8081

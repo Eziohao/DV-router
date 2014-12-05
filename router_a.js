ca8188dda2591c1709ef103566b65903871a742f
eziohao <eziohao@gmail.com>
2014-12-05 08:44:50 -0500

    add routing.js

--- router_a.js
+++ router_a.js
@@ -1,7 +1,7 @@
 var app = require('express')(); //get express model
 var http = require('http').Server(app); //create a http server
 var io = require('socket.io')(http); //create socket.io
-
+var route=require('./routing.js')
 var router_name = 'a'; //to store login router_name
 var router_port = 2547;
 var DV = {};
@@ -17,77 +17,8 @@ app.get('/', function(req, res) { //link the js to html file
 	res.sendfile('index.html');
 
 });
-//console.log(isEmpty(DV['d']));
-function routing(s_DV) {                   //routing 
-	if (!isEmpty(DV)) {
-		for (var item in s_DV) {
-			for (var i in DV) {
-				if (item != i && item != router_name && isEmpty(DV[item])&&!isEmpty(DV[s_DV[item].sID])) {
-					console.log('get it');
-					DV[item] = {
-						"sID": router_name,
-						"dID": s_DV[item].dID,
-						"dP": s_DV[item].dP,
-						"nH": parseInt(s_DV[item].nH)+parseInt(DV[s_DV[item].sID].nH),
-						"dis": parseInt(s_DV[item].dis)+parseInt(DV[s_DV[item].sID].nH) ,
-						"nR": s_DV[item].sID,
-						"sP":router_port
-					};
-				} else if (item != i && item != router_name && !isEmpty(DV[item])) {
-					console.log('have it');
-					if (DV[item].nR != s_DV[item].sID&&!isEmpty(DV[s_DV[item].sID])) {
-						if (parseInt(DV[item].dis) >= parseInt(s_DV[item].dis) + parseInt(DV[s_DV[item].sID].dis)) {
-							console.log('change cost');
-							DV[item].dis = parseInt(s_DV[item].sID) + parseInt(DV[s_DV[item].sID].dis);
-							DV[item].nH = DV[s_DV[item].sID].nH + s_DV[item].nH;
-							DV[item].nR = s_DV[item].sID;
-						}
-					}
-				}
-				else if(item==router_name&&isEmpty(DV[s_DV[item].sID])){
-					console.log('catch it');
-					DV[s_DV[item].sID]={
-    				    "sID": router_name,
-						"dID": s_DV[item].sID,
-						"dP": s_DV[item].sP,
-						"nH": s_DV[item].nH,
-						"dis": parseInt(s_DV[item].dis),
-						"nR": s_DV[item].sID,
-						"sP":router_port
-    			}
-				}
-			}
-		}
-	}
-    else{
-    	for(item in s_DV){
-    		if(item==router_name){
-    			DV[s_DV[item].sID]={
-    				    "sID": router_name,
-						"dID": s_DV[item].sID,
-						"dP": s_DV[item].sP,
-						"nH": s_DV[item].nH,
-						"dis": parseInt(s_DV[item].dis),
-						"nR": s_DV[item].sID,
-						"sP":router_port
-    			}
-    		}
-    		else if(item!=router_name&&!isEmpty(DV[s_DV[item].sID])){
-               DV[item]={
-               	        "sID": router_name,
-						"dID": item,
-						"dP": s_DV[item].sP,
-						"nH": parseInt(s_DV[item].nH)+parseInt(DV[s_DV[item].sID].nH),
-						"dis": parseInt(s_DV[item].dis)+parseInt(DV[s_DV[item].sID].dis),
-						"nR": s_DV[item].sID,
-						"sP":router_port
-               }
-    		}
-    	}
-    }
 
-};
-
+console.log(route.routing(DV,DV));
 
 
 function findrouters(name) {
@@ -100,29 +31,7 @@ function findrouters(name) {
 	}
 };
 
-function isEmpty(obj) {
-	for (var prop in obj) {
-		if (obj.hasOwnProperty(prop))
-			return false;
-	}
 
-	return true;
-}
-
-function send() {
-		if (!isEmpty(DV)) {
-			s = JSON.stringify(DV);
-			var copy = new Buffer(s);
-			for (item in DV) {
-				client.send(copy, 0, copy.length, DV[item].dP, '127.0.0.1', function(err, bytes) {
-					if (err) {
-						throw err;
-					}
-					client.close();
-				})
-			}
-		}
-	}
 
 
 io.on('connection', function(socket) { //if a user coonect the server
@@ -195,9 +104,7 @@ io.on('connection', function(socket) { //if a user coonect the server
     	msg=JSON.stringify(DV);
     	io.emit('display on',msg);
     })
-   console.log(temp);
-   console.log('change');
-   console.log(temp1);
+
 });
 server.on('listening', function() {
 	console.log('udp started');
@@ -206,12 +113,12 @@ server.on('message', function(message, rinfo) {
 	var s_DV = {};
 	s_DV = JSON.parse(message);
 	
-		routing(s_DV);
+		route.routing(s_DV);
 		console.log(DV);
 	
 })
 setInterval(function () {
-    if (!isEmpty(DV)) {
+    if (!route.isEmpty(DV)) {
 		s = JSON.stringify(DV);
 		var copy = new Buffer(s);
 		for(item in DV){
--- routing.js
+++ routing.js
@@ -1,69 +1,20 @@
-function routing(s_DV) {
-	if (!isEmpty(DV)) {
-		for (var item in s_DV) {
-			for (var i in DV) {
-				if (item != i && item != router_name && isEmpty(DV[item])&&!isEmpty(DV[s_DV[item.sID]])) {
-					console.log('get it');
-					DV[item] = {
-						"sID": router_name,
-						"dID": s_DV[item].dID,
-						"dP": s_DV[item].dP,
-						"nH": parseInt(s_DV[item].nH)+parseInt(DV[s_DV[item].sID].nH),
-						"dis": parseInt(s_DV[item].dis)+parseInt(DV[s_DV[item].sID].nH) ,
-						"nR": s_DV[item].sID,
-						"sP":router_port
-					};
-				} else if (item != i && item != router_name && !isEmpty(DV[item])) {
-					console.log('have it');
-					if (DV[item].nR != s_DV[item].sID&&!isEmpty(DV[s_DV[item].sID])) {
-						if (parseInt(DV[item].dis) >= parseInt(s_DV[item].dis) + parseInt(DV[s_DV[item].sID].dis)) {
-							console.log('change cost');
-							DV[item].dis = parseInt(s_DV[item].sID) + parseInt(DV[s_DV[item].sID].dis);
-							DV[item].nH = DV[s_DV[item].sID].nH + s_DV[item].nH;
-							DV[item].nR = s_DV[item].sID;
-						}
-					}
-				}
-				else if(item==router_name&&isEmpty(DV[s_DV[item].sID])){
-					console.log('catch it');
-					DV[s_DV[item].sID]={
-    				    "sID": router_name,
-						"dID": s_DV[item].sID,
-						"dP": s_DV[item].sP,
-						"nH": s_DV[item].nH,
-						"dis": parseInt(s_DV[item].dis),
-						"nR": s_DV[item].sID,
-						"sP":router_port
-    			}
-				}
-			}
-		}
-	}
-    else{
-    	for(item in s_DV){
-    		if(item==router_name){
-    			DV[s_DV[item].sID]={
-    				    "sID": router_name,
-						"dID": s_DV[item].sID,
-						"dP": s_DV[item].sP,
-						"nH": s_DV[item].nH,
-						"dis": parseInt(s_DV[item].dis),
-						"nR": s_DV[item].sID,
-						"sP":router_port
-    			}
-    		}
-    		else if(item!=router_name&&!isEmpty(DV[s_DV[item].sID])){
-               DV[item]={
-               	        "sID": router_name,
-						"dID": item,
-						"dP": s_DV[item].sP,
-						"nH": parseInt(s_DV[item].nH)+parseInt(DV[s_DV[item].sID].nH),
-						"dis": parseInt(s_DV[item].dis)+parseInt(DV[s_DV[item].sID].dis),
-						"nR": s_DV[item].sID,
-						"sP":router_port
-               }
-    		}
-    	}
-    }
+exports.routing=function (DV,s_DV,router_name,router_port) {
+	
 
 };
+exports.isEmpty=function(obj) {
+	for (var prop in obj) {
+		if (obj.hasOwnProperty(prop))
+			return false;
+	}
+
+	return true;
+}
+function isEmpty(obj) {
+	for (var prop in obj) {
+		if (obj.hasOwnProperty(prop))
+			return false;
+	}
+
+	return true;
+}
